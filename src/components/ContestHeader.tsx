ab4130f202fdbda87d209cd7061222d54d132a47
kiji-haru <kijiharu3112@outlook.jp>
2023-02-18 17:54:21 +0000

    chore/Linkの下線を消去

--- src/components/ContestHeader.tsx
+++ src/components/ContestHeader.tsx
@@ -12,6 +12,7 @@ const normal = {
     textAlign: "center",
     color: "#000",
     height: "1rem",
+    textDecoration: "none",
 }
 const current = {
     textAlign: "center",
@@ -19,6 +20,9 @@ const current = {
     height: "1.1rem",
     fontWeight: "bold",
     textDecoration: "underline",
+}
+const linkstyle = {
+    textDecoration: "none",
 }
 
 const ContestHeader: React.FC<Props> = ({page}) => {
@@ -32,10 +36,10 @@ const ContestHeader: React.FC<Props> = ({page}) => {
                     justifyContent: "center"
                 }}
             >
-                <Link to="/contestId/top"><Button><Typography sx={page === "top" ? current : normal}>説明</Typography></Button></Link>
-                <Link to="/contestId/problem"><Button><Typography sx={page === "problem" ? current : normal}>問題</Typography></Button></Link>
-                <Link to="/contestId/result"><Button><Typography sx={page === "result" ? current : normal}>提出結果</Typography></Button></Link>
-                <Link to="/contestId/ranking"><Button><Typography sx={page === "ranking" ? current : normal}>ランキング</Typography></Button></Link>
+                <Link to="/contestId/top" style={linkstyle}><Button><Typography sx={page === "top" ? current : normal}>説明</Typography></Button></Link>
+                <Link to="/contestId/problem"style={linkstyle}><Button><Typography sx={page === "problem" ? current : normal}>問題</Typography></Button></Link>
+                <Link to="/contestId/result"style={linkstyle}><Button><Typography sx={page === "result" ? current : normal}>提出結果</Typography></Button></Link>
+                <Link to="/contestId/ranking"style={linkstyle}><Button><Typography sx={page === "ranking" ? current : normal}>ランキング</Typography></Button></Link>
             </Stack>
         </center>
     )
