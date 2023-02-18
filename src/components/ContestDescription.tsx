fb049134d09db5d1364b7bec8340afc100fc1eb4
kiji-haru <kijiharu3112@outlook.jp>
2023-02-18 17:48:21 +0000

    feat/ContestTop

---
+++ src/components/ContestDescription.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import MarkdownIt from "markdown-it";
+import mk from "markdown-it-katex";
+
+const md = new MarkdownIt();
+md.use(mk);
+
+interface Props {
+    description: string;
+}
+
+const ContestDescription: React.FC<Props> = ({ description }) => {
+    const text = md.render(description);
+    return <div dangerouslySetInnerHTML={{ __html: text }} />;
+};
+
+export default ContestDescription;
