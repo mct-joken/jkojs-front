e7ead650c2eb8d719375c0c84db46c3b419eb9c1
kiji-haru <kijiharu3112@outlook.jp>
2023-03-04 02:24:07 +0900

    eslintとprettierの導入と各箇所への適用をした

--- src/route.tsx
+++ src/route.tsx
@@ -1,33 +1,36 @@
-import React from "react"
+import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./component/Header";
 import Top from "./pages/top";
 import Contests from "./pages/contests";
-import {ThemeProvider} from "@mui/system";
+import { ThemeProvider } from "@mui/system";
 import theme from "./fixtures/theme";
 import Mypage from "./pages/mypage";
 import ContestTop from "./pages/contestTop";
 import Contest from "./pages/contest";
 import Problem from "./pages/problem";
 
-export const Router:React.FC = () => {
-    return (
-        <>
-            <ThemeProvider theme={theme}>
-                <BrowserRouter >
-                    <Header userIcon="" userName="your name"　isLogin={false}/>
-                    <Routes>
-                        <Route path="/" element={<Top/>}/>
-                        <Route path="/contests" element={<Contests/>}/>
-                        <Route path="/mypage" element={<Mypage/>}/>
-                        <Route　path=":contestId" element={<Contest　title={"テスト用コンテスト"}/>}>
-                            <Route path="top" element={<ContestTop　/>}/>
-                            <Route path="problem" element={<Problem/>}/>
-                        </Route>
-                        <Route path="*" element={<div>404</div>}/>
-                    </Routes>
-                </BrowserRouter>
-            </ThemeProvider>
-        </>
-    )
-}
+export const Router: React.FC = () => {
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>
+          <Header userIcon="" userName="your name" isLogin={false} />
+          <Routes>
+            <Route path="/" element={<Top />} />
+            <Route path="/contests" element={<Contests />} />
+            <Route path="/mypage" element={<Mypage />} />
+            <Route
+              path=":contestId"
+              element={<Contest title={"テスト用コンテスト"} />}
+            >
+              <Route path="top" element={<ContestTop />} />
+              <Route path="problem" element={<Problem />} />
+            </Route>
+            <Route path="*" element={<div>404</div>} />
+          </Routes>
+        </BrowserRouter>
+      </ThemeProvider>
+    </>
+  );
+};
